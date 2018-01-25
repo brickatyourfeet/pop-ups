44a4d584fc38ae67a3d44df803ea0c7f889381ce
brickatyourfeet <brickatyourfeet@gmail.com>
2018-01-25 11:31:40 -0800

    commit before auto-linking libraries

--- src/store/config.js
+++ src/store/config.js
@@ -6,8 +6,15 @@ const rootReducer = combineReducers({
     popups: bigReducer
 })
 
+let composeEnhancers = compose
+
+if(__DEV__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 const config = () => {
-    return createStore(rootReducer)
+    return createStore(rootReducer, composeEnhancers())
+    //adding this guy lets us include 'enhancers' or middleware & redux devtools
 }
 
 export default config
