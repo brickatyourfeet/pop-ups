4d2c721a05f90ac617fb27d59310d9a4f8b92f53
brickatyourfeet <brickatyourfeet@gmail.com>
2018-01-31 08:54:37 -0800

    fix css for spot input

--- src/components/SpotInput/SpotInput.js
+++ src/components/SpotInput/SpotInput.js
@@ -4,17 +4,22 @@ import { View, Button, TextInput, StyleSheet} from 'react-native'
 import DefaultInput from '../UIComponents/DefaultInput'
 
 const SpotInput = props => (
-        <View>
+        <View style={styles.container}>
         <DefaultInput placeholder='Popup Title' value={props.spot} onChangeText={props.onChangeText}/>
-
+        <DefaultInput placeholder='Start: MM/DD/YYYY' value={props.start} onChangeText={props.onChangeText}/>
+        <DefaultInput placeholder='End: MM/DD/YYYY' value={props.end} onChangeText={props.onChangeText}/>
+        <DefaultInput placeholder='Web/Info link:' value={props.info} onChangeText={props.onChangeText}/>
 
         </View>
         )
 
 
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    alignItems: 'center'
+  }
+})
 
 export default SpotInput
 
-// <DefaultInput placeholder='Start: MM/DD/YYYY' value={props.start} onChangeText={props.onChangeText}/>
-// <DefaultInput placeholder='End: MM/DD/YYYY' value={props.end} onChangeText={props.onChangeText}/>
-// <DefaultInput placeholder='Web/Info link:' value={props.info} onChangeText={props.onChangeText}/>
