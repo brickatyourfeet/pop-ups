85d431633dfbd6bbbb4d84e38ad8400f3433c587
brickatyourfeet <brickatyourfeet@gmail.com>
2018-04-23 18:13:35 -0700

    re-add auth action

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,31 @@
 import { SUBMIT_ATTEMPT } from './types'
 
 export const submitAttempt = (authData) => {
-    return {
-        type: SUBMIT_ATTEMPT,
-        authData: authData
+    return dispatch => {
+        dispatch(authSignup(authData))
+    }
+}
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        fetch('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyAPGVeDasm4S7x0IGL8Txpph7crqztSdUk', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            alert('authentication failed. please try again.')
+        })
+        .then(res => res.json)
+        .then(parsedRes => {
+            console.log(parsedRes)
+        })
     }
 }
